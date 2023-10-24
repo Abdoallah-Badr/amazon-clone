4f3a3f47ad561a0c31199216d02aec64a65f955c
Abdoallah-Badr <bedobadr53@gmail.com>
2023-10-24 11:40:21 +0300

    add redux-persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,18 @@ import RootLayout from "@/components/RootLayout";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "@/store/store";
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store} >
-      <div className="font-bodyFont">
-        <RootLayout>
-          <Component {...pageProps} />
-        </RootLayout>
-      </div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <div className="font-bodyFont">
+          <RootLayout>
+            <Component {...pageProps} />
+          </RootLayout>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
